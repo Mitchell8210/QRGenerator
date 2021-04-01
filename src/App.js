a862cfb4e7e223d162cb613faa6c8dbba2fa97c9
Mitchell Rhoads <mrhoads@tunego.com>
2021-04-01 10:47:15 -0700

    first commit, set up simple qr generator by text

--- src/App.js
+++ src/App.js
@@ -1,22 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import QRCode from "qrcode";
+
+import "./App.css";
 
 function App() {
+  const [value, setValue] = useState("");
+  const [error, setError] = useState(false);
+  const [qrUrl, setQRUrl] = useState("");
+  const generate = () => {
+    if (value !== "") {
+      QRCode.toDataURL(value)
+        .then((url) => {
+          console.log(url);
+          setQRUrl(url);
+        })
+        .catch((err) => {
+          console.error(err);
+          setError(err);
+          return err;
+        });
+      setValue("");
+    } else {
+      setError(true);
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="title">QR Generator</div>
+        <div className="subTitle">
+          This site will let you generate a QR Code
+        </div>
+        <div className="box">
+          {error && <div className="error">Please enter some text</div>}
+          <input
+            type="text"
+            className="input"
+            value={value}
+            onChange={(e) => {
+              setValue(e.target.value);
+              setError(false);
+              setQRUrl("");
+            }}
+          />
+          <button onClick={() => generate()} className="button">
+            Submit
+          </button>
+          {qrUrl && (
+            <div className="img">
+              <img src={qrUrl} width="150px" />
+            </div>
+          )}
+        </div>
       </header>
     </div>
   );
